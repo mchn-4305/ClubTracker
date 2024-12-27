f06afa14e56d93678188986da03e3730ce4cbc4f
Micah Chen <micahchen34@gmail.com>
2024-12-27 11:18:37 -0800

    feat: set up LogIn skeleton

--- packages/frontend/src/pages/LogIn.jsx
+++ packages/frontend/src/pages/LogIn.jsx
@@ -1,1 +1,17 @@
-import React from "react";
+// src/pages/LogIn.jsx
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+// import LogInForm from "./LogInForm";
+// import styles from "./LogIn.module.css";
+
+const LogIn = ( {onLogin} ) => {
+    return (
+        <h1>LogIn</h1>
+        // <div className="container">
+        //     <h1>Club<span>Tracker</span></h1>
+        //     <LogInForm></LogInForm>
+        // </div>
+    );
+};
+
+export default LogIn;
